f50a54bbc4ea0d47efc1cb612bfd39040d5c631f
Davide Doronzo <davide.doronzo@gmail.com>
2022-09-08 02:43:45 +0100

    Update pod start script

--- home-server-pod-start.sh
+++ home-server-pod-start.sh
@@ -2,4 +2,7 @@
 
 cd "${0%/*}"
 
+podman machine list | grep home-server-vm || podman machine init home-server-vm --cpus=1 --memory=1024
+podman machine inspect home-server-vm | grep '"State": "running"' || podman machine start home-server-vm
+envsubst < $PWD/home-server-manifest.yaml | podman play kube --down - || true
 envsubst < $PWD/home-server-manifest.yaml | podman play kube -
