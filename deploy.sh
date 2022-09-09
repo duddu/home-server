5005e1db4da6f81eaf00ae3ec1e07f146592fbb9
Davide Doronzo <duddu@users.noreply.github.com>
2022-09-09 14:28:52 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,6 @@ export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 
 rm -rf ~/.home-server
 git clone git@github.com:duddu/podman-home-server-pod.git ~/.home-server
-cd oweurhfioeurh
 cd ~/.home-server
 git-crypt unlock
 HOME=/Users/duddu ./home-server-pod-start.sh
