6ab773c9bdd791a3d747324540a8cff28bc42606
Davide Doronzo <duddu@users.noreply.github.com>
2022-09-09 15:00:40 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,10 @@
 set -e
 
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+CLONE_DIR="~/.home-server"
 
-rm -rf ~/.home-server
-git clone git@github.com:duddu/podman-home-server-pod.git ~/.home-server
-cd ~/.home-server
+rm -rf $CLONE_DIR
+git clone --depth 1 git@github.com:duddu/podman-home-server-pod.git $CLONE_DIR
+cd $CLONE_DIR
 git-crypt unlock
 HOME=/Users/duddu ./home-server-pod-start.sh
