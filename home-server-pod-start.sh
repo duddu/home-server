355af9611018ee4211e96fbe1134fb9859bcdb77
Davide Doronzo <duddu@users.noreply.github.com>
2022-09-10 12:05:18 +0100

    Update home-server-pod-start.sh

--- home-server-pod-start.sh
+++ home-server-pod-start.sh
@@ -1,8 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 cd "${0%/*}"
 
-podman machine list | grep home-server-vm || podman machine init home-server-vm --cpus=1 --memory=1024
-podman machine inspect home-server-vm | grep '"State": "running"' || podman machine start home-server-vm
-envsubst < $PWD/home-server-manifest.yaml | podman play kube --down - || true
-envsubst < $PWD/home-server-manifest.yaml | podman play kube -
+VM=home-server-vm
+VM_CPUS=1
+VM_RAM=1024
+MANIFEST=$(envsubst < $PWD/home-server-manifest.yaml)
+
+podman machine list | grep $VM &&
+  echo "⏭ Virtual machine ${VM} already exists" ||
+  echo "⏳ Creating virtual machine ${VM}..." &&
+    podman machine init $VM --cpus=$VM_CPUS --memory=$VM_RAM &&
+    echo "✅ Virtual machine ${VM} created successfully"
+
+podman machine inspect $VM | grep '"State": "running"' &&
+  echo "⏭ Virtual machine ${VM} is already running" ||
+  echo "⏳ Starting virtual machine ${VM}..." &&
+    podman machine start $VM &&
+    echo "🚀 Virtual machine ${VM} started successfully"
+
+echo "⏳ Tearing down pod home-server if running..."
+$MANIFEST | podman play kube --down - &&
+  echo "✅ Torn down pod home-server" ||
+  echo "⏭ Pod home-server is not running"
+
+echo "⏳ Starting pod home-server..."
+$MANIFEST | podman play kube - &&
+  echo "🚀 Pod home-server started successfully"
