68327dd7feb50b33a9d40532c518202f6442c8bc
Davide Doronzo <davide.doronzo@gmail.com>
2022-09-24 14:29:23 +0100

    Improve processes test output message

--- packages/rust-api/src/routes/processes/test.rs
+++ packages/rust-api/src/routes/processes/test.rs
@@ -5,7 +5,7 @@ mod integration_test {
     use super::rocket;
     use rocket::local::blocking::Client;
     use rocket::http::{ContentType, Status};
-    use rocket::serde::json::serde_json::to_vec;
+    use rocket::serde::json::serde_json;
     use std::env;
     use crate::routes::processes::{self, Process};
 
@@ -39,13 +39,16 @@ mod integration_test {
             }
         ];
 
-        env::set_var("CONTAINERS_HOST", "127.0.0.2");
+        env::set_var(processes::ENV_KEY_HOST, "127.0.0.2");
 
         let client = Client::tracked(rocket()).expect("valid rocket instance");
         let response = client.get(uri!(processes::get_processes)).dispatch();
 
         assert_eq!(response.status(), Status::Ok);
         assert_eq!(response.content_type().unwrap(), ContentType::JSON);
-        assert_eq!(response.into_bytes().unwrap(), to_vec(&expected_response).unwrap());
+        assert_eq!(
+            String::from_utf8(response.into_bytes().unwrap()),
+            String::from_utf8(serde_json::to_vec(&expected_response).unwrap())
+        );
     }
 }
