cc87f9a6df03a5263b6089db778a6b9020e643ed
Davide Doronzo <davide.doronzo@gmail.com>
2022-09-26 10:08:30 +0100

    Update health check path

--- scripts/home-server-health-check.sh
+++ scripts/home-server-health-check.sh
@@ -3,6 +3,6 @@
 set -e
 
 echo "⏳ Running e2e health check..."
-(curl --cert $SSL_CLIENT_CERT_PATH --fail $HTTPS_HOST/health 1> /dev/null &&
+(curl --cert $SSL_CLIENT_CERT_PATH --fail $HTTPS_HOST/api/health 1> /dev/null &&
   echo "✅ Health check completed") ||
   exit 1
