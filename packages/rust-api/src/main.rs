df6da9d5dc385318099cb711a0749de879fb3b5e
Davide Doronzo <davide.doronzo@gmail.com>
2022-09-26 10:05:06 +0100

    Add api base path

--- packages/rust-api/src/main.rs
+++ packages/rust-api/src/main.rs
@@ -1,10 +1,12 @@
 #[macro_use] extern crate rocket;
 mod routes;
 
+const BASE_PATH: &str = "/api";
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![
+        .mount(BASE_PATH, routes![
             routes::health_check::get_health_check,
             routes::processes::get_processes,
             routes::version::get_version
