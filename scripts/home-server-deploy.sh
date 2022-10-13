f4ee587566984bb53e19c4f738fc7f65967ea070
Davide Doronzo <davide.doronzo@gmail.com>
2022-10-13 17:48:57 +0100

    ci(scripts): update repo name in git clone address

--- scripts/home-server-deploy.sh
+++ scripts/home-server-deploy.sh
@@ -7,7 +7,7 @@ export HOME="/Users/duddu"
 CLONE_DIR="${HOME}/.home-server"
 
 rm -rf $CLONE_DIR
-git clone --depth 1 git@github.com:duddu/podman-home-server-pod.git $CLONE_DIR
+git clone --depth 1 git@github.com:duddu/home-server.git $CLONE_DIR
 cd $CLONE_DIR
 git-crypt unlock
 bash ./scripts/home-server-pod-start.sh
