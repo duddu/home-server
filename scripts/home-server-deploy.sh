e36fd7a2de1f9cfd15cb914c10c56e4211962d92
Davide Doronzo <davide.doronzo@gmail.com>
2022-10-13 20:38:38 +0100

    ci(scripts): update deploy script sparse checkout

--- scripts/home-server-deploy.sh
+++ scripts/home-server-deploy.sh
@@ -7,9 +7,17 @@ export HOME="/Users/duddu"
 CLONE_DIR="${HOME}/.home-server"
 
 rm -rf $CLONE_DIR
-git clone --depth 1 git@github.com:duddu/home-server.git $CLONE_DIR
+git clone --depth=20 --filter=blob:none --no-checkout git@github.com:duddu/home-server.git $CLONE_DIR
 cd $CLONE_DIR
+git sparse-checkout set --no-cone \
+    .git-crypt \
+    .gitattributes \
+    home-server-manifest.yaml \
+    packages/nginx-reverse-proxy/ssl \
+    'scripts/home-server-*.sh'
+git reset --hard "${COMMIT_SHA:-HEAD}"
 git-crypt unlock
+
 if [ "$1" = "--machine-rm" ]
 then
     bash ./scripts/home-server-machine-rm.sh
