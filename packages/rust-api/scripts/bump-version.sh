7e848458653285b1d688267133ab7090f9985fb0
Davide Doronzo <davide.doronzo@gmail.com>
2022-10-15 00:07:02 +0100

    build(api): point to exact image tag

--- packages/rust-api/scripts/bump-version.sh
+++ packages/rust-api/scripts/bump-version.sh
@@ -12,5 +12,6 @@ new_version=$(semver bump $1 $current_version)
 sed -i '' -E "s/version = \"${current_version}\"/version = \"${new_version}\"/g" Cargo.toml
 cargo check
 sed -i '' -E "s/\/${current_version}\"/\/${new_version}\"/g" Rocket.toml
+sed -i '' -E "s/rust-api:${current_version}/rust-api:${new_version}/g" ../../home-server-manifest.yaml
 printf "${new_version}" > VERSION
 echo "✅ rust-api version bumped from ${current_version} to ${new_version}"
