5842ee03f8cea9bc3910eb1503d6cda794386a00
Davide Doronzo <davide.doronzo@gmail.com>
2022-10-16 19:37:42 +0100

    build(podman): improve rust api image directories

--- packages/rust-api/scripts/docker-entrypoint.sh
+++ packages/rust-api/scripts/docker-entrypoint.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-(./websocketd --port=8889 --devconsole tail -f rust-api.log) & 
-script -fq -c ./home-server-rust-api rust-api.log
+(websocketd --port=8889 --devconsole tail -f rust-api.log) & 
+script -fq -c home-server-rust-api rust-api.log
