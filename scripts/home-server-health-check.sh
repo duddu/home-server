14a8dd22f7cc361d54d09c22b1297dca3504d942
Davide Doronzo <davide.doronzo@gmail.com>
2022-10-17 15:55:23 +0100

    ci(scripts): add timeout to e2e health checks

--- scripts/home-server-health-check.sh
+++ scripts/home-server-health-check.sh
@@ -5,13 +5,15 @@ set -u
 : "${SSL_CLIENT_CERT_PATH:?Variable not set or empty}"
 : "${DOMAIN_NAME:?Variable not set or empty}"
 
+TIMEOUT=10
+
 perform_health_check () {
-  [ "$(curl -s --cert $SSL_CLIENT_CERT_PATH --fail https://$DOMAIN_NAME$1)" = "OK" ]
+  [ "$(curl -s --fail -m $TIMEOUT --cert $SSL_CLIENT_CERT_PATH https://$DOMAIN_NAME$1)" = "OK" ] 1> /dev/null
 }
 
-(echo "⏳ Running nginx health check..." &&
-  perform_health_check /health 1> /dev/null &&
-    echo "⏳ Running api health check..." &&
-      perform_health_check /api/health 1> /dev/null &&
+(echo "⏳ Running nginx e2e health check..." &&
+  perform_health_check /health &&
+    echo "⏳ Running api e2e health check..." &&
+      perform_health_check /api/health &&
   echo "🏥 Health checks completed") ||
   exit 1
