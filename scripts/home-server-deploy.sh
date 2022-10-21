a90091911a10b46194a842660403b5bed08bf179
Davide Doronzo <davide.doronzo@gmail.com>
2022-10-21 02:42:39 +0100

    ci(scripts): create home-server directory if missing

--- scripts/home-server-deploy.sh
+++ scripts/home-server-deploy.sh
@@ -6,22 +6,23 @@ export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 export HOME="/Users/${USER}"
 CLONE_DIR="${HOME}/.home-server"
 
+mkdir -p $CLONE_DIR
 rm -rf $CLONE_DIR/{*,.git*}
 cd $CLONE_DIR
 git clone -q --no-checkout --depth=1 --filter=blob:none git@github.com:duddu/home-server.git .
 git sparse-checkout set --no-cone \
-    .git-crypt \
-    .gitattributes \
-    home-server-manifest.yaml \
-    packages/nginx-reverse-proxy/ssl/certs \
-    packages/nginx-reverse-proxy/ssl/private \
-    'scripts/home-server-*.sh' \
-    1> /dev/null
+  .git-crypt \
+  .gitattributes \
+  home-server-manifest.yaml \
+  packages/nginx-reverse-proxy/ssl/certs \
+  packages/nginx-reverse-proxy/ssl/private \
+  'scripts/home-server-*.sh' \
+  1> /dev/null
 git reset --hard "${COMMIT_SHA:-HEAD}" 1> /dev/null
 git-crypt unlock
 
 if [ "$1" = "--machine-rm" ]
 then
-    bash ./scripts/home-server-machine-rm.sh
+  bash ./scripts/home-server-machine-rm.sh
 fi
 bash ./scripts/home-server-pod-start.sh
