baac82b657826565a2927a0592f7be6162e476f9
Davide Doronzo <davide.doronzo@gmail.com>
2022-11-25 05:49:57 +0000

    ci(podman): simplify machine start

--- scripts/podman/machine-start.sh
+++ scripts/podman/machine-start.sh
@@ -7,10 +7,10 @@ VM=home-server-vm
 (podman machine list | grep -q $VM &&
   echo "⏭ Virtual machine ${VM} exists") ||
   (echo "⏳ Creating virtual machine ${VM}..." &&
-    podman machine init $VM --rootful && sleep 3 &&
+    podman machine init $VM --rootful && sleep 5 &&
     echo "✨ Virtual machine ${VM} created successfully")
 
-(podman machine inspect $VM | grep -q '"State": "running"' &&
+(podman machine list | grep -qe "${VM}.*Currently running" &&
   echo "⏭ Virtual machine ${VM} is running") ||
   (echo "⏳ Starting virtual machine ${VM}..." &&
     podman machine start $VM &&
