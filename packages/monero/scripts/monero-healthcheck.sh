099a118cbbd1fab4acbed87a628cb8eee4a5ed59
Davide Doronzo <davide.doronzo@gmail.com>
2022-11-27 15:58:34 +0000

    fix(monero): health check command

--- packages/monero/scripts/monero-healthcheck.sh
+++ packages/monero/scripts/monero-healthcheck.sh
@@ -4,8 +4,4 @@ set -e
 set -u
 : "${MONERO_RPC_LOGIN:?Variable not set or empty}"
 
-(curl -ks https://localhost:18082 1> /dev/null &&
-  echo "Monero rpc ssl reachable") ||
-  (echo "Monero rpc ssl not reachable; trying to retrieve daemon status..." &&
-    monerod status --rpc-login $MONERO_RPC_LOGIN --log-level 1 ;
-    exit 1)
+monerod status --rpc-login $MONERO_RPC_LOGIN --log-level 1
