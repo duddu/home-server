ae263f13e317059cb3fac52f8df7630d85b10572
Davide Doronzo <davide.doronzo@gmail.com>
2022-11-28 13:48:30 +0000

    build(google-ddns): split image entrypoint and cmd

--- packages/google-ddns/scripts/nic-update.sh
+++ packages/google-ddns/scripts/nic-update.sh
@@ -2,15 +2,17 @@
 
 set -e
 set -u
-: "${TAG:?Variable not set or empty}"
 : "${GOOGLE_DDNS_HOSTNAME:?Variable not set or empty}"
 : "${GOOGLE_DDNS_LOGIN:?Variable not set or empty}"
+: "${TAG:?Variable not set or empty}"
+
+echo $(date +'%Y-%m-%dT%H:%M:%S%z') 'Google DDNS update started'
 
 PUBLIC_IP_DIR=/var/public-ip
 PUBLIC_IP=$(curl --fail -s https://domains.google.com/checkip)
 
 if [ -f "$PUBLIC_IP_DIR/$PUBLIC_IP" ]; then
-  echo "⏭ Public IP $PUBLIC_IP has not changed; skipping update."
+  echo "⏭ Public IP $PUBLIC_IP has not changed; skipping nic update"
   exit 0
 fi
 
@@ -18,13 +20,13 @@ echo "⏳ Public IP has changed to $PUBLIC_IP; attempting update..."
 
 NIC_UPDATE_PATH="domains.google.com/nic/update"
 NIC_UPDATE_QUERY="hostname=$GOOGLE_DDNS_HOSTNAME&myip=$PUBLIC_IP"
-NIC_UPDATE_URL="https://$GOOGLE_DDNS_LOGIN?$NIC_UPDATE_QUERY"
+NIC_UPDATE_URL="https://$GOOGLE_DDNS_LOGIN@$NIC_UPDATE_PATH?$NIC_UPDATE_QUERY"
 
-curl -X POST -v $NIC_UPDATE_URL \
+curl -X POST $NIC_UPDATE_URL \
   -H "Authorisation: Basic base64-encoded-auth-string" \
   -H "User-Agent: home-server/google-ddns/$TAG"
 
 rm -f $PUBLIC_IP_DIR/*
 touch $PUBLIC_IP_DIR/$PUBLIC_IP
 
-echo "✅ Dynamic DNS updated successfully."
+echo "✅ Dynamic DNS updated successfully"
