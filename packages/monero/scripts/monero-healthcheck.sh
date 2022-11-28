a7a36fcf04d1233a65252e021b979a2affcc756f
Davide Doronzo <davide.doronzo@gmail.com>
2022-11-28 20:01:17 +0000

    fix(monero): improve healthcheck error logs

--- packages/monero/scripts/monero-healthcheck.sh
+++ packages/monero/scripts/monero-healthcheck.sh
@@ -4,4 +4,10 @@ set -e
 set -u
 : "${MONERO_RPC_LOGIN:?Variable not set or empty}"
 
-monerod status --rpc-login $MONERO_RPC_LOGIN --log-level 1
+CMD_OUTPUT=$(monerod status --rpc-login $MONERO_RPC_LOGIN --log-level 1)
+
+(echo $CMD_OUTPUT | grep -q 'uptime' &&
+  echo "Monero daemon healthy") ||
+  (echo "Monero daemon unhealthy" &&
+    echo $CMD_OUTPUT ;
+    exit 1)
