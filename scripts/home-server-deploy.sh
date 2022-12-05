f273f9163cba70af90ef03eb328af24eac2f2c25
Davide Doronzo <davide.doronzo@gmail.com>
2022-12-05 22:48:48 +0000

    ci(scripts): speed up deploy

--- scripts/home-server-deploy.sh
+++ scripts/home-server-deploy.sh
@@ -3,61 +3,68 @@
 set -e
 set -u
 : "${USER:?Variable not set or empty}"
-: "${GIT_REF:-main}"
 
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 export HOME="/Users/$USER"
-export GIT_REF
+local REPO="git@github.com:duddu/home-server.git"
+local CLONE_DIR="$HOME/.home-server"
 
-clone_sparse_checkout () {
-  git clone -q --depth=1 --no-checkout --filter=blob:none git@github.com:duddu/home-server.git . > /dev/null
+prepare_dir () {
+  echo "⏳ Preparing directory..."
+  rm -rf $CLONE_DIR
+  mkdir -p $CLONE_DIR
+  cd $CLONE_DIR
+  git clone -q --depth=1 --no-checkout --filter=blob:none $REPO .
   git sparse-checkout set --no-cone \
     .git-crypt \
     .gitattributes \
     'config/**' \
     'packages/**/VERSION' \
     'scripts/**'
-  git reset --hard $GIT_REF
+  git reset --hard ${GIT_REF:-origin/main}
+  git-crypt unlock
+  printf $(git rev-parse HEAD) > COMMIT_SHA
+  rm -rf .git*
 }
-
-local CLONE_DIR="$HOME/.home-server"
-rm -rf $CLONE_DIR
-mkdir -p $CLONE_DIR
-cd $CLONE_DIR
-clone_sparse_checkout
-git-crypt unlock
-export COMMIT_SHA=$(git rev-parse HEAD)
-printf $COMMIT_SHA > COMMIT_SHA
-rm -rf .git*
 
 run_script () {
-  zsh ./scripts/$1.sh
+  zsh $CLONE_DIR/scripts/$1.sh
 }
 
-# run part of the block also if machine/cluster config changed
-# (save hashes before running sparse checkout)
-if [ "${1:-}" = "--restart-vm" ]
-then
-  echo "⏳ Recreating virtual machine and cluster..."
-  run_script kind/cluster-delete
-  run_script docker/stop
-fi
-run_script docker/start
-run_script kind/cluster-create
+prepare_cluster () {
+  if [ "${1:-}" = "--restart-vm" ]
+  then
+    echo "⏳ Recreating virtual machine and cluster..."
+    run_script kind/cluster-delete
+    run_script docker/stop
+  fi
+  if ! kubectl get nodes | grep -q Ready
+  then
+    run_script docker/start
+    run_script kind/cluster-create
+  fi
+}
 
-for version in ./packages/**/VERSION(.); do
-  local package=$(printf $version | cut -d / -f 3 | sed 's/-/_/g' | tr a-z A-Z)
-  export declare ${package}_VERSION=$(cat $version)
-done
+apply_k8s_resources () {
+  for version in $CLONE_DIR/packages/**/VERSION(.); do
+    local package=$(printf $version | cut -d / -f 6 | sed 's/-/_/g' | tr a-z A-Z)
+    export declare ${package}_VERSION=$(cat $version)
+  done
 
-echo "⏳ Preparing resources..."
-cd ./config/k8s
-kubectl config set-context --current --namespace=home-server
-envsubst < kustomization.tmpl.yaml > kustomization.yaml
-kubectl apply -k .
-echo "⏳ Waiting for pods..."
-sleep 3
-kubectl wait --for=condition=Available --timeout=240s deployments --all &&
-  kubectl wait --for=condition=ContainersReady --field-selector=spec.restartPolicy=Always --timeout=120s pods ||
-  (kubectl get deployments ; kubectl describe pods ; exit 1)
-echo "🚀 Pods ready"
+  echo "⏳ Preparing resources..."
+  cd $CLONE_DIR/config/k8s
+  kubectl config set-context --current --namespace=home-server
+  envsubst < kustomization.tmpl.yaml > kustomization.yaml
+  kubectl apply -k .
+
+  echo "⏳ Waiting for pods..."
+  sleep 3
+  kubectl wait --for=condition=Available --timeout=240s deployments --all &&
+    kubectl wait --for=condition=ContainersReady --field-selector=spec.restartPolicy=Always --timeout=120s pods ||
+    (kubectl get deployments ; kubectl describe pods ; exit 1)
+  echo "🚀 Pods ready"
+}
+
+prepare_dir
+prepare_cluster
+apply_k8s_resources
